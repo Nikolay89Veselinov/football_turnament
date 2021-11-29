7056b56ec61f726ec8ca15827afcb3f6f7cf1f4e
Nikolay <Nikolay89Veselinov.com>
2021-11-29 10:13:20 +0200

    Created football_team model

--- football_turnament/football_teams/models.py
+++ football_turnament/football_teams/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class FootballTeam(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Football team'
+        verbose_name_plural = 'Football teams'
+
+    def __str__(self):
+        return self.name
