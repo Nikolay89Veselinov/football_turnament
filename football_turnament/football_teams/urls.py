a3ef51420daeec093953d09bb6146aa903dedbb7
Nikolay <Nikolay89Veselinov.com>
2021-11-30 15:33:11 +0200

    Created list, create and filter views

--- football_turnament/football_teams/urls.py
+++ football_turnament/football_teams/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.views.generic import  ListView
 
-from .views import create_football_team
-
+from .views import create_football_team, filter_matches
+from .models import FootballTeam, Matches
 
 app_name = 'football_teams'
 urlpatterns = [
+    path('list_team/', ListView.as_view(model=FootballTeam), name="list_team",),
     path('create/', create_football_team, name="create",),
+    path('filter_matches/', filter_matches, name="filter_matches",),
+    path('matches/', ListView.as_view(model=Matches), name="matches",),
 ]
--- football_turnament/football_teams/views.py
+++ football_turnament/football_teams/views.py
@@ -1,6 +1,10 @@
+import json
+from datetime import datetime
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import FootballTeamForm
+from .models import Matches
 
 
 def create_football_team(request):
@@ -16,4 +20,25 @@ def create_football_team(request):
         'form': form,
     }
     return render(request, 'football_team.html', context)
+
+
+def filter_matches(request):
+    context = {}
+    data = []
+    if request.is_ajax():
+        date = request.GET.get('match_date')
+        if date != 'empty':
+            date_dt3 = datetime.strptime(date, '%Y-%m-%d')
+            matches_of_the_day = Matches.objects.filter(date_created=date_dt3.date()).all()
+            for match in matches_of_the_day:
+                data.append({
+                    'first_team': match.first_team,
+                    'second_team': match.second_team
+                })
+            json_content = json.dumps(data).encode('utf-8')
+            return HttpResponse(json_content, 'applications/json; charset=utf-8')
+    context.update({
+        'matches': Matches.objects.all()
+    })
+    return render(request, 'filter_matches.html', context)
     
