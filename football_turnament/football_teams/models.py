d5348dba7f714a52253b98b0861db78b4e9d8ef8
Nikolay <Nikolay89Veselinov.com>
2021-11-30 12:43:39 +0200

    Created Matches model

--- football_turnament/football_teams/models.py
+++ football_turnament/football_teams/models.py
@@ -10,3 +10,9 @@ class FootballTeam(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Matches(models.Model):
+    first_team = models.CharField(max_length=50)
+    second_team = models.CharField(max_length=50)
+    date_created = models.DateField(auto_now_add=True)
